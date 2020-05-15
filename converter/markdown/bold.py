7ac4fd755102a9fbd2dd4804d32fdff511e08631
achetporov <achetporov@codio.com>
2020-05-15 11:03:22 +0300

    - escaped asterisk in T-bold font - clearing spaces in italic font

--- converter/markdown/bold.py
+++ converter/markdown/bold.py
@@ -9,12 +9,13 @@ class Bold(object):
 
     def convert(self):
         output = self.str
+        escaped_asterisk = "\\*"
         output = re.sub(r"\\textbf{(.*?)}", r"**\1**", output, flags=re.DOTALL + re.VERBOSE)
         output = re.sub(r"{\\bf[ ](.*?)}", r"**\1**", output, flags=re.DOTALL + re.VERBOSE)
         output = re.sub(r"{\\sf[ ](.*?)}", r"**\1**", output, flags=re.DOTALL + re.VERBOSE)
 
         output = match_block("\\B{", output, lambda match: f"**{match}**")
         output = match_block("\\C{", output, lambda match: f"__{match}__")
-        output = match_block("\\T{", output, lambda match: f"**{match}**")
+        output = match_block("\\T{", output, lambda match: f"**{match.strip().replace('*', escaped_asterisk)}**")
 
         return output
