0179caa90eb23d29a018de4fffa49dc19f01fdf5
achetporov <achetporov@codio.com>
2020-06-01 17:07:08 +0300

    fixed chips link

--- converter/markdown/chips.py
+++ converter/markdown/chips.py
@@ -2,8 +2,8 @@ import re
 
 from converter.markdown.text_as_paragraph import TextAsParagraph
 
-chips_re = re.compile(r"""\\begin{chips}{(?P<title>.*?)}(?P<block_contents>.*?)\\end{chips}""",
-                      flags=re.DOTALL + re.VERBOSE)
+chips_re = re.compile(r"""\\begin{chips}{(?P<title>.*?)}{(?P<ref>.*?)}(?P<block_contents>.*?)\\end{chips}""",
+                      flags=re.DOTALL)
 
 
 class Chips(TextAsParagraph):
@@ -15,8 +15,9 @@ class Chips(TextAsParagraph):
         block_contents = self.to_paragraph(block_contents)
         title = matchobj.group('title')
         title = self.to_paragraph(title)
+        ref = matchobj.group('ref')
         caret_token = self._caret_token
-        return f'## {title}{caret_token}{block_contents}{caret_token}'
+        return f'## {title}{caret_token}{ref}{block_contents}{caret_token}'
 
     def convert(self):
         return chips_re.sub(self.make_block, self.str)
