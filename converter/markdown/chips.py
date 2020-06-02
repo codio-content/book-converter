4742304d5dc519c8d941bf926bd8319628c2ed87
achetporov <achetporov@codio.com>
2020-06-02 11:13:08 +0300

    fix chips without href

--- converter/markdown/chips.py
+++ converter/markdown/chips.py
@@ -2,7 +2,7 @@ import re
 
 from converter.markdown.text_as_paragraph import TextAsParagraph
 
-chips_re = re.compile(r"""\\begin{chips}{(?P<title>.*?)}{(?P<ref>.*?)}(?P<block_contents>.*?)\\end{chips}""",
+chips_re = re.compile(r"""\\begin{chips}{(?P<title>.*?)}({(?P<ref>.*?)})?(?P<block_contents>.*?)\\end{chips}""",
                       flags=re.DOTALL)
 
 
@@ -11,13 +11,15 @@ class Chips(TextAsParagraph):
         super().__init__(latex_str, caret_token)
 
     def make_block(self, matchobj):
+        caret_token = self._caret_token
         block_contents = matchobj.group('block_contents')
         block_contents = self.to_paragraph(block_contents)
         title = matchobj.group('title')
         title = self.to_paragraph(title)
         ref = matchobj.group('ref')
-        caret_token = self._caret_token
-        return f'## {title}{caret_token}{ref}{caret_token}{caret_token}{block_contents}{caret_token}'
+        if ref is not None:
+            return f'## {title}{caret_token}{ref}{caret_token}{caret_token}{block_contents}{caret_token}'
+        return f'## {title}{caret_token}{caret_token}{block_contents}{caret_token}'
 
     def convert(self):
         return chips_re.sub(self.make_block, self.str)
