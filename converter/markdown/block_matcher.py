868c309b1ac8627c49cbf7a598580cd4b7e12a23
achetporov <achetporov@codio.com>
2020-06-02 18:31:03 +0300

    fixed escaped brackets in block_matcher

--- converter/markdown/block_matcher.py
+++ converter/markdown/block_matcher.py
@@ -4,14 +4,15 @@ def match_block(chars, output, repl_func):
         level = 0 if '{' in chars else 1
         for index in range(pos + len(chars), len(output), 1):
             ch = output[index]
-            if ch == '}':
+            prev_char = output[index - 1]
+            if ch == '}' and prev_char != '\\':
                 if level == 0:
                     start_position = pos+len(chars) if '{' in chars else output.find("{", pos) + 1
                     output = output[0:pos] + repl_func(output[start_position:index]) + output[index + 1:]
                     break
                 else:
                     level += 1
-            elif ch == '{':
+            elif ch == '{' and prev_char != '\\':
                 level -= 1
         pos = output.find(chars)
     return output
