46c48dc60dccc7477e69d22164ad1c5c18bbfd67
achetporov <achetporov@codio.com>
2020-06-04 10:18:11 +0300

    add book name to yaml

--- converter/convert.py
+++ converter/convert.py
@@ -185,7 +185,7 @@ def prepare_structure(generate_dir):
 
 def make_metadata_items(config):
     book = {
-        "name": "TODO: book name",
+        "name": config.get("name"),
         "children": []
     }
     metadata = {
--- converter/toc.py
+++ converter/toc.py
@@ -249,7 +249,8 @@ def get_bookdown_toc(folder, name):
 
 def print_to_yaml(structure, tex, bookdown=False):
     file_format = "bookdown: {}".format(tex.name) if bookdown else "tex: {}".format(tex.name)
-    yaml_structure = """workspace:
+    yaml_structure = """name: "Engineering Software As a Service (2nd edition)"
+workspace:
   directory: {}
   {}
 assets:
