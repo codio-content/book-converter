445c5e01ec3976e014d76bf3be1e42541af644e4
achetporov <achetporov@codio.com>
2020-06-08 15:39:30 +0300

    change list regex

--- converter/rst2markdown.py
+++ converter/rst2markdown.py
@@ -12,7 +12,7 @@ class Rst2Markdown(object):
         self._heading3_re = re.compile(r"""^(?P<content>.*?\n)?(?:~)+\s*$""", flags=re.MULTILINE)
         self._heading4_re = re.compile(r"""^(?P<content>.*?\n)?(?:")+\s*$""", flags=re.MULTILINE)
         self._num_list_re = re.compile(r"""^#\.(?P<content>.*?)\s*?$""", flags=re.MULTILINE)
-        self._list_re = re.compile(r"""^(?P<content>\* *[\s\S]+?)\n$""", flags=re.MULTILINE)
+        self._list_re = re.compile(r"""^(?P<content>\* .+?)\n$""", flags=re.MULTILINE + re.DOTALL)
         self._ext_links_re = re.compile(r"""`(?P<name>.*?)\n?<(?P<ref>https?:.*?)>`_""")
         self._ref_re = re.compile(r""":ref:`(?P<name>.*?)(?P<label_name><.*?>)?`""")
         self._term_re = re.compile(r""":term:`(?P<name>.*?)(<(?P<label_name>.*?)>)?`""")
