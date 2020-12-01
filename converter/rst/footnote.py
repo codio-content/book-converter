b074e549a98ea3bdd39ae2298916a2b44e47ea77
achetporov <achetporov@codio.com>
2020-12-01 13:37:16 +0300

    add biblio footnote

--- converter/rst/footnote.py
+++ converter/rst/footnote.py
@@ -6,7 +6,11 @@ class Footnote(object):
         self.str = source_string
         self._footnote_counter = 0
         self._marker_footnote_re = re.compile(r"""\[#]_""")
+        self._bib_footnote_re = re.compile(r"""\[(.*?)]_""")
         self._footnote_re = re.compile(r"""^\.\. \[#] (?P<content>(?:[^\n]+\n)*)""", flags=re.MULTILINE)
+
+    def _bib_foot_note(self, matchobj):
+        return f'<span style="color: #0c3762;">**[{matchobj.group(1)}]**</span>'
 
     def _foot_note_marker(self, matchobj):
         self._footnote_counter += 1
@@ -26,4 +30,5 @@ class Footnote(object):
         output = self._marker_footnote_re.sub(self._foot_note_marker, output)
         self._footnote_counter = 0
         output = self._footnote_re.sub(self._foot_note, output)
+        output = self._bib_footnote_re.sub(self._bib_foot_note, output)
         return output
