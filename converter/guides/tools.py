31186d9ba267d283eeae1e6694b7100af5b6480c
Dmitrii Suchkov <dsuchkov@codio.com>
2021-01-28 17:32:31 +0300

    ensure_ascii in json dumps (#73)

--- converter/guides/tools.py
+++ converter/guides/tools.py
@@ -13,7 +13,7 @@ def write_file(file_path, content):
 
 
 def write_json(file_path, json_data):
-    write_file(file_path, json.dumps(json_data, sort_keys=True, indent=2, separators=(',', ': ')))
+    write_file(file_path, json.dumps(json_data, sort_keys=True, indent=2, separators=(',', ': '), ensure_ascii=False))
 
 
 def read_file(file_path):
