e9489c31e64b78f01d06d207ec4d3d2ae5eb6943
achetporov <achetporov@codio.com>
2021-05-12 12:22:34 +0300

    fix comment regex

--- converter/rst/comment.py
+++ converter/rst/comment.py
@@ -4,7 +4,7 @@ import re
 class Comment(object):
     def __init__(self, source_string):
         self.str = source_string
-        self._comment_re = re.compile(r"""\.\.\s+[^\n]+(?:(?:(?!\n^$|\n\S)\s+[^\n]+\n*(?=\n\S))*)""",
+        self._comment_re = re.compile(r"""^\.\.\s+[^\n]+(?:(?:(?!\n^$|\n\S)\s+[^\n]+\n*(?=\n\S))*)""",
                                       flags=re.MULTILINE)
 
     @staticmethod
