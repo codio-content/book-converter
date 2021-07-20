aacd4d754404a4caea2b4bb455ae4fa0449b2296
achetporov <achetporov@codio.com>
2021-07-20 21:52:31 +0300

    refactor

--- esaas-mc-converter.py
+++ esaas-mc-converter.py
@@ -53,7 +53,7 @@ def get_section_item(name, files):
     }
 
 
-def get_assessment_item(assessment, name, file_name, count):
+def get_assessment_item(assessment, name, file_name, exercise_num):
     instructions = ''
     guidance = []
     answers = []
@@ -80,13 +80,9 @@ def get_assessment_item(assessment, name, file_name, count):
         if match_guidance:
             guidance.append(match_guidance.group('explanation'))
 
-        option_value = re.sub(r", :explanation => (?P<explanation>.*?)$", "", option_value, flags=re.MULTILINE)
-
-        option_value = re.sub(r"%[qQ]{(.*?)}", r"\1", option_value + '\n', flags=re.MULTILINE + re.DOTALL + re.VERBOSE)
-
-        option_value = option_value.strip().strip('\"')
         if option_type == 'group':
             continue
+
         if option_type == 'tags':
             tags_list = option_value.split(',')
             for tag in tags_list:
@@ -95,17 +91,23 @@ def get_assessment_item(assessment, name, file_name, count):
                     tag = match_tag.group('tag_value')
                     tags.append({'name': 'topic', 'value': tag})
             continue
+
+        option_value = re.sub(r", :explanation => (?P<explanation>.*?)$", "", option_value, flags=re.MULTILINE)
+        option_value = re.sub(r"%[qQ]{(.*?)}", r"\1", option_value + '\n', flags=re.MULTILINE + re.DOTALL + re.VERBOSE)
+        option_value = option_value.strip().strip('\"')
+
         if option_type == 'text':
             instructions = option_value
             continue
+
         if option_type == 'answer' or option_type == 'distractor':
             is_correct = option_type == 'answer'
             answers.append(get_answer(option_value, is_correct))
 
     if assessment.type == CHOICE_ANSWER or assessment.type == SELECT_MULTIPLE:
-        return get_multiple_choice_structure(name, count, instructions, assessment, answers, guidance, tags)
+        return get_multiple_choice_structure(name, exercise_num, instructions, assessment, answers, guidance, tags)
     if assessment.type == FILL_IN_BLANK:
-        return get_fill_in_blank_structure(name, count, instructions, assessment, answers, guidance, tags)
+        return get_fill_in_blank_structure(name, exercise_num, instructions, assessment, answers, guidance, tags)
 
 
 def get_answer(answer, is_correct):
@@ -116,14 +118,14 @@ def get_answer(answer, is_correct):
     }
 
 
-def get_multiple_choice_structure(name, count, instructions, assessment, answers, guidance, tags):
+def get_multiple_choice_structure(name, exercise_num, instructions, assessment, answers, guidance, tags):
     correct_answers_count = sum(map(lambda item: item.get('correct'), answers))
     multipleResponse = assessment.type == SELECT_MULTIPLE or correct_answers_count > 1
     return {
         "type": "multiple-choice",
-        "taskId": f"multiple-choice-{slugify(name)}-{count}",
+        "taskId": f"multiple-choice-{slugify(name)}-{exercise_num}",
         "source": {
-            "name": f"{name} {count}",
+            "name": f"{name} {exercise_num}",
             "showName": True,
             "instructions": instructions.replace("\\n", "\n"),
             "multipleResponse": multipleResponse,
@@ -148,13 +150,13 @@ def get_multiple_choice_structure(name, count, instructions, assessment, answers
     }
 
 
-def get_fill_in_blank_structure(name, count, instructions, assessment, answers, guidance, tags):
+def get_fill_in_blank_structure(name, exercise_num, instructions, assessment, answers, guidance, tags):
     answer = answers[0].get('answer').strip()
     return {
         "type": "fill-in-the-blanks",
-        "taskId": f"fill-in-the-blanks-{slugify(name)}-{count}",
+        "taskId": f"fill-in-the-blanks-{slugify(name)}-{exercise_num}",
         "source": {
-            "name": f"{name} {count}",
+            "name": f"{name} {exercise_num}",
             "showName": True,
             "instructions": instructions.replace("\\n", "\n"),
             "showValues": False,
@@ -220,25 +222,25 @@ def convert_to_codio_structure(item):
     structure = []
     sections = []
     assessments = []
-
-    book_item = get_book_item(item.name, PAGE)
-    structure.append(book_item)
-
     files = [
         {
             "action": "close",
             "path": "#tabs"
         }
     ]
+
+    book_item = get_book_item(item.name, PAGE)
+    structure.append(book_item)
+
     content = generate_content(item.name, item.assessment_items)
     current_item = get_section_item(item.name, files)
     current_item['content-file'] = '\n'.join(content)
     sections.append(current_item)
 
-    assessment_count = 0
+    exercise_num = 0
     for assessment in item.assessment_items:
-        assessment_count += 1
-        assessments.append(get_assessment_item(assessment, item.name, item.file_name, assessment_count))
+        exercise_num += 1
+        assessments.append(get_assessment_item(assessment, item.name, item.file_name, exercise_num))
 
     return structure, sections, assessments
 
@@ -257,29 +259,28 @@ def get_data_to_process(base_directory):
     to_process = []
     for file in (base_directory.glob('*.rb')):
         assessment_items = []
-        file_name_without_ext = file.name.rsplit(".", 1)[0]
 
+        file_name_without_ext = file.name.rsplit(".", 1)[0]
         file_data = read_file(file.resolve())
 
-        match_data = re.search(r"^quiz\s+['\"](?P<name>.*?)['\"]\s+(?:do)?\n(?P<assessments_block>.*(?=end))",
+        match_quiz_data = re.search(r"^quiz\s+['\"](?P<name>.*?)['\"]\s+(?:do)?\n(?P<assessments_block>.*(?=end))",
                                file_data, flags=re.MULTILINE + re.DOTALL)
-
-        if not match_data:
+        if not match_quiz_data :
             return
-        name = match_data.group('name').strip()
-        assessments_block = match_data.group('assessments_block')
+        chapter_name = match_quiz_data .group('name').strip()
+        assessments_block = match_quiz_data .group('assessments_block')
         assessments_block = re.sub(r"^\s+#+$", "", assessments_block, flags=re.MULTILINE)
         assessments_block += "\n"
 
-        result = re.finditer(r"^\s+(?P<type>choice_answer|select_multiple|fill_in)(?P<settings>\s+:.*? => ?.*?)?\s+do\n"
+        exercise_blocks = re.finditer(r"^\s+(?P<type>choice_answer|select_multiple|fill_in)(?P<settings>\s+:.*? => ?.*?)?\s+do\n"
                              r"(?P<content>.*?)(?:\s+end\s+\n(?!\n\s*\S)|(?=\s+\1))",
                              assessments_block, flags=re.MULTILINE + re.DOTALL + re.VERBOSE)
-        if not result:
+        if not exercise_blocks:
             print(file, 'PARSE ERROR')
             return
         print(file)
 
-        for item in list(result):
+        for item in list(exercise_blocks):
             mc_type = item.group('type')
             content = item.group('content')
 
@@ -297,7 +298,7 @@ def get_data_to_process(base_directory):
                                  flags=re.MULTILINE + re.DOTALL + re.VERBOSE)
             assessment_items.append(AssessmentItem(mc_type, options, settings))
 
-        to_process.append(FileToProcess(name, file_name_without_ext, assessment_items))
+        to_process.append(FileToProcess(chapter_name, file_name_without_ext, assessment_items))
     return to_process
 
 
