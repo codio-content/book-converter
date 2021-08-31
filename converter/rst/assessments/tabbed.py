34c09fc801bd0e3a9eb49aaec08724767f6e0706
achetporov <achetporov@codio.com>
2021-08-31 15:53:46 +0300

    tabbed

--- converter/rst/assessments/tabbed.py
+++ converter/rst/assessments/tabbed.py
@@ -14,15 +14,16 @@ class Tabbed(object):
         tab_question_match = re.search(r"""\.\.\s+tab::\s+(?P<type>.*?)\n(?P<ex_content>.*?)\n(?=\s*.. tab:: Answer)""",
                                        content, flags=re.MULTILINE + re.DOTALL)
         if tab_question_match:
+            str_len = 0
             cut_content = []
             ex_content = tab_question_match.group('ex_content').rstrip()
             content_list = ex_content.split('\n')
             content_list = [item for item in content_list if item.strip() != '']
             for ind, item in enumerate(content_list):
-                indent_match = re.search(r'^( *)\.\.', content_list[0])
+                indent_match = re.search(r'^( *)\.\. ', item, flags=re.MULTILINE)
                 if indent_match:
                     str_len = len(indent_match.group(1))
-                    cut_content.append(content_list[ind][str_len:])
+                cut_content.append(content_list[ind][str_len:])
             final_content = '\n'.join(cut_content)
             return f'{final_content}\n\n'
 
