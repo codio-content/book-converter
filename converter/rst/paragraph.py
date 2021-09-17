dc566cee6a2e7b9f529f9c4056aad6f394aa766c
achetporov <achetporov@codio.com>
2021-09-17 21:36:16 +0300

    fix paragraph

--- converter/rst/paragraph.py
+++ converter/rst/paragraph.py
@@ -4,7 +4,7 @@ import re
 class Paragraph(object):
     def __init__(self, source_string):
         self.str = source_string
-        self._paragraph_re = re.compile(r"""(?!\s|#\. |\* |- |\d\. |\.\. |```|\|\|\|)(^(?P<content>.*?)\n?(?=^$))""",
+        self._paragraph_re = re.compile(r"""(?!\s|#\. |\* |- |\d\. |\.\. |```|\|\|\|)(^(?P<content>.*?)\n?(?=^ *$))""",
                                         flags=re.MULTILINE + re.DOTALL)
 
     @staticmethod
