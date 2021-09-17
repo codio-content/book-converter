76b661ad3f2fd1008137820e2c7e4db94c5f3ffe
achetporov <achetporov@codio.com>
2021-09-17 19:09:49 +0300

    sup

--- converter/rst/math.py
+++ converter/rst/math.py
@@ -5,6 +5,7 @@ class Math(object):
     def __init__(self, source_string):
         self.str = source_string
         self._math_re = re.compile(r""":math:`(?P<content>.*?)`""")
+        self._sup_re = re.compile(r"""([\S\d]+)\\ :sup:`(.*?)`""")
 
     @staticmethod
     def _math(matchobj):
@@ -16,7 +17,12 @@ class Math(object):
         content = content.replace("\\+", "+")
         return f'${content}$'
 
+    @staticmethod
+    def _sup(matchobj):
+        return f'${matchobj.group(1)}^{{{matchobj.group(2)}}}$'
+
     def convert(self):
         output = self.str
         output = self._math_re.sub(self._math, output)
+        output = self._sup_re.sub(self._sup, output)
         return output
