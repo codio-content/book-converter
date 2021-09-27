bc00181eece83ece39c9a4dbe7d66a105cb7ea59
achetporov <achetporov@codio.com>
2021-09-27 23:42:17 +0300

    clean indention fix

--- converter/rst/utils/clean_indention.py
+++ converter/rst/utils/clean_indention.py
@@ -2,13 +2,17 @@ import re
 
 
 def clean_indention(content):
-    str_len = 0
+    start_space_len = 0
     cut_content = []
     content = content.lstrip('\n').rstrip()
     content_list = content.split('\n')
     indent_match = re.search(r'^ *', content_list[0])
     if indent_match:
-        str_len = len(indent_match.group(0))
+        start_space_len = len(indent_match.group(0))
     for ind, item in enumerate(content_list):
-        cut_content.append(content_list[ind][str_len-1:])
+        offset = 1
+        item_space_len = len(re.search(r'^ *', item).group(0))
+        if item_space_len < start_space_len and item_space_len != 0:
+            offset = (start_space_len + 1) - item_space_len
+        cut_content.append(content_list[ind][start_space_len - offset:])
     return '\n'.join(cut_content)
