ab8979f96fca7318ca31df20c8d524cebed3c828
achetporov <achetporov@codio.com>
2021-10-04 18:56:31 +0300

    fix paragraph regex

--- converter/rst/paragraph.py
+++ converter/rst/paragraph.py
@@ -4,14 +4,14 @@ import re
 class Paragraph(object):
     def __init__(self, source_string):
         self.str = source_string
-        self._paragraph_re = re.compile(r"""(?!\s|#\. |\* |- |\d\. |\.\. |```|\|\|\|)(^(?P<content>.*?)\n?(?=^ *$))""",
-                                        flags=re.MULTILINE)
+        self._paragraph_re = re.compile(r"""(?!\s|#\. |\* |- |\d\. |\.\.\s+|```|\|\|\|)^(?P<content>.+?)\n\s*\n""",
+                                        flags=re.MULTILINE + re.DOTALL)
 
     @staticmethod
     def _paragraph(matchobj):
         content = matchobj.group('content')
         content = content.replace('\n', ' ')
-        return f'{content}\n'
+        return f'{content}\n\n'
 
     def convert(self):
         output = self.str
