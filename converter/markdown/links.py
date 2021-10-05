d4ba09a2caccc6b10935083852b7c87b19d4dc76
achetporov <achetporov@codio.com>
2021-10-05 11:29:42 +0300

    fix href

--- converter/markdown/links.py
+++ converter/markdown/links.py
@@ -11,7 +11,7 @@ class Links(object):
         output = re.sub(r"\\weblink{(.*?)}([\s%])?{(.*?)}", r"[\3](\1)", output, flags=re.DOTALL + re.VERBOSE)
         output = re.sub(r"\\weblink{(.*?)}", r"[\1](\1)", output, flags=re.MULTILINE)
         output = re.sub(r"\\url{(.*?)}", r"[\1](\1)", output)
-        output = re.sub(r"\\href{(.*?)}{(\\[a-z]+)?\s?(.*?)}", r"[\1](\3)", output)
-        output = re.sub(r"\\href{(.*?)}`(\\[a-z]+)?\s?(.*?)`", r"[\1](\3)", output)
+        output = re.sub(r"\\href{(.*?)}{(\\[a-z]+)?\s?(.*?)}", r"[\3](\1)", output)
+        output = re.sub(r"\\href{(.*?)}`(\\[a-z]+)?\s?(.*?)`", r"[\3](\1)", output)
 
         return output
