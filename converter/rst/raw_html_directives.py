0eea4680fe4ebe294d944f7835a75a7b217d1b4d
achetporov <achetporov@codio.com>
2021-10-06 22:28:26 +0300

    raw_html nested

--- converter/rst/raw_html_directives.py
+++ converter/rst/raw_html_directives.py
@@ -8,14 +8,17 @@ class RawHtmlDirectives(object):
         self.str = source_string
         self._caret_token = caret_token
         self._links = list()
+
         self._raw_html_re = re.compile(
-            r"""^(?P<indent>[ ]*)(?P<dash>^[ ]+-[ ]+)?\.\.[ ]+(?:\|(?P<tag>[.,~:;!@#$%^&*()\-+=\w\s]+)\|[ ]+)?
+            r"""^\.\.[ ]+(?:\|(?P<tag>[.()\w\s]+)\|[ ]+)?raw::[ ]html\n(?P<content>.*?)\n(?=\S|(?![^$]+$))""",
+            flags=re.MULTILINE + re.DOTALL)
+
+        self._raw_html_nested_re = re.compile(
+            r"""^(?P<indent>[ ]*)(?P<dash>^[ ]+-[ ]+)\.\.[ ]+(?:\|(?P<tag>[.,~:;!@#$%^&*()\-+=\w\s]+)\|[ ]+)?
             raw::[ ]html\n.*?\n(?P<content>\s*.*?)\n(?=\S|^[ ]*$)""", flags=re.MULTILINE + re.DOTALL + re.VERBOSE)
 
     def _raw_html(self, matchobj):
         tag = matchobj.group('tag')
-        indent = matchobj.group('indent')
-        dash_indent = matchobj.group('dash')
         content = matchobj.group('content').strip()
         if tag:
             tag = tag.strip()
@@ -24,11 +27,22 @@ class RawHtmlDirectives(object):
         else:
             split_content = [f' {item.lstrip()}' for item in content.split('\n')]
             final_content = '\n'.join(split_content)
-            if not indent and not dash_indent:
-                return final_content
-            dash_indent = dash_indent or ''
-            return f'{dash_indent}{final_content}'
+            final_content = re.sub('<pre>', '<pre>\n\n', final_content)
+            return f'{final_content}\n\n'
+
+    def _raw_html_nested(self, matchobj):
+        indent = matchobj.group('indent')
+        dash_indent = matchobj.group('dash')
+        content = matchobj.group('content').strip()
+        split_content = [f' {item.lstrip()}' for item in content.split('\n')]
+        final_content = '\n'.join(split_content)
+        if not indent and not dash_indent:
+            return final_content
+        dash_indent = dash_indent or ''
+        return f'{dash_indent}{final_content}'
 
     def convert(self):
-        output = self._raw_html_re.sub(self._raw_html, self.str)
+        output = self.str
+        output = self._raw_html_re.sub(self._raw_html, output)
+        output = self._raw_html_nested_re.sub(self._raw_html_nested, output)
         return output, self._links
