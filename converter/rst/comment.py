b10cef5eb09f65fb41a7c64a8359dc2033415cd3
achetporov <achetporov@codio.com>
2021-10-07 23:33:01 +0300

    fix topic paragraph

--- converter/rst/comment.py
+++ converter/rst/comment.py
@@ -4,7 +4,7 @@ import re
 class Comment(object):
     def __init__(self, source_string):
         self.str = source_string
-        self._comment_re = re.compile(r"""^\.\. +\.\. +.*?\n(?=\S)|\.\. .*?\n$""", flags=re.MULTILINE + re.DOTALL)
+        self._comment_re = re.compile(r"""^\.\. +\.\. +.*?\n(?=\S)|^\.\. .*?\n$""", flags=re.MULTILINE + re.DOTALL)
 
     @staticmethod
     def _comment(matchobj):
--- converter/rst/topic.py
+++ converter/rst/topic.py
@@ -1,11 +1,10 @@
 import re
 
-from converter.markdown.text_as_paragraph import TextAsParagraph
+from converter.rst.paragraph import Paragraph
 
 
-class Topic(TextAsParagraph):
+class Topic(object):
     def __init__(self, source_string, caret_token):
-        super().__init__(source_string, caret_token)
         self.str = source_string
         self._caret_token = caret_token
         self._topic_re = re.compile(
@@ -18,8 +17,9 @@ class Topic(TextAsParagraph):
         topic_type = matchobj.group('type')
         content = matchobj.group('content')
         content = re.sub(r"\n +", "\n ", content)
-        content = content.lstrip()
-        content = self.to_paragraph(content)
+        content = [item.lstrip() for item in content.split('\n')]
+        content = '\n'.join(content)
+        content = Paragraph(content).convert()
 
         figure_number = matchobj.group('figure_number') if matchobj.group('figure_number') is not None else ''
         figure_number = '' if 'Proof' in topic_type else f' {figure_number}'
