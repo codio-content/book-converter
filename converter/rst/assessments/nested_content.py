1b91ce3ec06a7f55c27d1da2b0d39bfecc2c1ddf
achetporov <achetporov@codio.com>
2021-11-15 17:29:55 +0300

    nested content fix

--- converter/rst/assessments/nested_content.py
+++ converter/rst/assessments/nested_content.py
@@ -6,16 +6,17 @@ class NestedContent(object):
         self.str = source_string
         self._caret_token = caret_token
         self._timed_re = re.compile(
-            r"""^\.\.[ ]+(timed|reveal)::[ ]+(?P<name>[^\n]+)\n((?: +:[^\n]+\n)+)?(?P<content>.*?)\n(?=\S|(?!^$)$)""",
-            flags=re.MULTILINE + re.DOTALL)
+            r"""^\.\.[ ]+(timed|reveal)::[ ]+(?P<name>[^\n]+)\n((?:(?P<indent>[ ]+):[^\n]+\n)+)?
+            (?P<content>.*?)\n(?=\S|(?!^$)$)""", flags=re.MULTILINE + re.DOTALL + re.VERBOSE)
 
     def _timed(self, matchobj):
         cut_content = []
         content = matchobj.group('content').lstrip('\n').rstrip()
+        indent = len(matchobj.group('indent')) if matchobj.group('indent') else 3
         content_list = content.split('\n')
         for ind, item in enumerate(content_list):
-            if re.search(r'^[ ]{3}', item):
-                item = item[3:]
+            if re.search(fr'^[ ]{{{indent}}}', item):
+                item = item[indent:]
             cut_content.append(item)
         return '\n'.join(cut_content) + '\n\n'
 
