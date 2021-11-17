699a7242cd7cb29e2eda8c01a582e5bf5a55a6d1
achetporov <achetporov@codio.com>
2021-11-17 11:21:40 +0300

    comments

--- converter/rst/comment.py
+++ converter/rst/comment.py
@@ -4,8 +4,8 @@ import re
 class Comment(object):
     def __init__(self, source_string):
         self.str = source_string
-        self._comment_re = re.compile(r"""^\.\.[ ]+\.\.[ ]+.*?\n(?=\S|(?!^$)[ ]*$)""", flags=re.MULTILINE + re.DOTALL)
-        self._comment_directives_re = re.compile(r"""^\.\.[ ]+(?:[^\n]+\n)+""", flags=re.MULTILINE)
+        self._comment1_re = re.compile(r"""^\.\.[ ]+\.\.[ ]+.*?\n(?=\S|(?!^$)$)""", flags=re.MULTILINE + re.DOTALL)
+        self._comment2_re = re.compile(r"""^\.\.[ ]+(?:[^\n]+\n)+""", flags=re.MULTILINE)
 
     @staticmethod
     def _comment(matchobj):
@@ -13,6 +13,6 @@ class Comment(object):
 
     def convert(self):
         output = self.str
-        output = self._comment_re.sub(self._comment, output)
-        output = self._comment_directives_re.sub(self._comment, output)
+        output = self._comment1_re.sub(self._comment, output)
+        output = self._comment2_re.sub(self._comment, output)
         return output
